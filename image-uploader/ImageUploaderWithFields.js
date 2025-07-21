f3d0cd0aaf6f3905e889337a3d3997dc0bf3a0e4
laomi233 <454182465@qq.com>
2025-07-22 00:05:30 +0800

    test

--- image-uploader/ImageUploaderWithFields.js
+++ image-uploader/ImageUploaderWithFields.js
@@ -1,29 +1,21 @@
-// src/components/ImageUploaderWithFields.js
-import React from 'react';
-import { MapTo, withConditionalPlaceHolder } from '@adobe/aem-react-editable-components';
-
-const ImageUploaderWithFields = (props) => {
-  const { fileReference, title, description } = props;
-
-  return (
-    <div className="image-uploader-with-fields" style={{ maxWidth: '600px' }}>
-      {fileReference ? (
-        <img src={fileReference} alt={title || 'Image'} style={{ width: '100%', marginBottom: '10px' }} />
-      ) : (
-        <div style={{ backgroundColor: '#eee', padding: '20px', textAlign: 'center' }}>No image uploaded</div>
-      )}
-
-      {title && <h2>{title}</h2>}
-      {description && <p>{description}</p>}
-    </div>
-  );
-};
-
-const EditConfig = {
-  emptyLabel: 'Image with Fields',
-  isEmpty: (props) => {
-    return !props || (!props.fileReference && !props.title && !props.description);
-  },
-};
-
-MapTo('myproject/components/image-uploader')(withConditionalPlaceHolder(ImageUploaderWithFields, EditConfig));
+const ImageWithCustomFields = (props) => {
+    const { fileReference, custom } = props;
+    const title = custom?.title;
+    const description = custom?.description;
+  
+    return (
+      <div>
+        {fileReference && <img src={fileReference} alt={title || 'Image'} />}
+        {title && <h3>{title}</h3>}
+        {description && <p>{description}</p>}
+      </div>
+    );
+  };
+  
+  const EditConfig = {
+    emptyLabel: 'Image with Fields',
+    isEmpty: props => !props?.fileReference && !props?.custom?.title && !props?.custom?.description,
+  };
+  
+  MapTo('myproject/components/image-with-fields')(withConditionalPlaceHolder(ImageWithCustomFields, EditConfig));
+  
